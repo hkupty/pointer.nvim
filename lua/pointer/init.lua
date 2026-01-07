1986610293b7f31811a5bb24f61041d7d2a0ba8f
Henry Kupty <hkupty@gmail.com>
2026-01-07 15:03:29 +0100

    refactor: Start anew

--- lua/pointer/init.lua
+++ lua/pointer/init.lua
@@ -1,72 +1,42 @@
 -- luacheck: globals vim
-local utils = require("pointer.utils")
-local sinks = require("pointer.sinks")
-local formatters = require("pointer.formatters")
-local sources = require("pointer.sources")
 
+local pointer = {}
 
-local pointer = {
-  data = {}
-}
+pointer.config = {}
 
-pointer.definition = function(project)
-  return utils.get(pointer.data, project[#project]) or
-      utils.get(pointer.data, project[#project - 1]) or
-      pointer.data
+pointer.setup = function(opts) end
+
+pointer.locate = function()
+	local mode, blocking = vim.api.nvim_get_mode()
+
+	if blocking then
+		return
+	end
+
+	local file = vim.fn.expand("%:p")
+	local base = vim.fn.expand("%:p:h")
+	local root = vim.trim(vim.system("git rev-parse --show-toplevel", {
+		cwd = base,
+	}))
+
+	if ~vim.startswith(file, root) then
+		vim.notify("File does not belong to git repo, somehow", vim.log.levels.ERROR)
+		return
+	end
+
+	local path = file:sub(#root + 2)
+
+	if vim.startswith(mode, "n") then
+		local cursor = vim.api.nvim_win_get_cursor(0)
+
+		-- NOTE: single-line locate
+		return { path, cursor[1] }
+	end
+
+	-- TODO: multi-line locate
+
+	-- HACK: Fallback case, no line found
+	return { path }
 end
-
-
-pointer.bind = function(opts)
-  local source = opts.source or pointer.data.source or sources.from_cursor
-  local sink = opts.sink or pointer.data.sink or sinks.to_clip
-  local formatter = opts.formatter or pointer.data.formatter or formatters.url
-
-  return function()
-    pointer.run(source, formatter, sink)
-  end
-end
-
-pointer.run = function(source, formatter, sink)
-  local data = {}
-  for key, srcfn in pairs(source) do
-    data[key] = srcfn()
-  end
-
-  if type(formatter) == "table" then
-    formatter = formatter[data.remote]
-  end
-
-  sink(formatter(data))
-end
-
-pointer.config = function(config)
-  pointer.data = utils.safe_merge({}, config)
-end
-
-local default_mapping = {
-  url = { "yu", formatters.url },
-  proj = { "ypp", formatters.path.project_path },
-  root = { "yRp", formatters.path.root_path },
-}
-
-pointer.setup = function(mapping)
-  local mappings = utils.safe_merge(default_mapping, mapping or {})
-  for _, map in pairs(mappings) do
-    vim.keymap.set(
-      { "n" },
-      map[1],
-      pointer.bind { formatter = map[2], source = sources.from_cursor },
-      { silent = true }
-    )
-
-    vim.keymap.set(
-      { "v" },
-      map[1],
-      pointer.bind { formatter = map[2], source = sources.from_visual },
-      { silent = true }
-    )
-  end
-end
-
 
 return pointer
